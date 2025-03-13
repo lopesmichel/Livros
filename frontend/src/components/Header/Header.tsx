9db4b008d6c71ef25191f77b269d34618fb8cd38
Michel Lopes <Michel.lopes@krona.net>
2025-03-13 08:13:22 -0300

    services

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -14,7 +14,7 @@ export function Header() {
         <HeaderTitle />
         <div className="mt-28">
           <p className="md:text-7xl text-3xl font-bold text-evergreen">
-            Encontre livros <br /> que seja a sua acara!
+            Encontre livros <br /> que seja a sua cara!
           </p>
           <p className="text-gray-500 my-5 text-xl">Lorem ipsum</p>
           <Button title="Pesquisar livros" />
